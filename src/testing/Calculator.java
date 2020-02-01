09722176a67e2e1bc19736042521b198d559b7b9
umangsv <umangsv1987@gmail,com>
2020-02-01 21:47:11 +0530

    commiting calculator

---
+++ src/testing/Calculator.java
@@ -0,0 +1,25 @@
+package testing;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return (a+b);
+	}
+	
+	public int sub(int a, int b) {
+		return (a+b);
+	}
+	
+	public int mul(int a, int b) {
+		return (a*b);
+	}
+	
+	public int div(int a, int b) {
+		return (a/b);
+	}
+	
+	public int square(int a) {
+		return (a*a);
+	}
+	
+}
